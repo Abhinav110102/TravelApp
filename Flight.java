1ba88d6c02b0aa2b29f19fd8d05b4f08dcacd6b0
Abhinav110102 <abhinavmyadala@gmail.com>
2022-03-14 22:58:11 -0400

    commit 2

--- Flight.java
+++ Flight.java
@@ -1,3 +1,32 @@
+import java.util.ArrayList;
+
 public class Flight {
-    
+// - duration: double
+// - startLocation: Location
+// - endLocation: Location
+// - luggage: ArrayList<Luggage>
+// - seating: ArrayList<Seat> 
+// - available: boolean
+// -  flightType: String
+private  double duration;
+private Location startLocation;
+private Location endLocation;
+private ArrayList<Luggage> luggage;
+private ArrayList<Seat> seating ;
+private boolean available;
+private String flightType ;
+
+// + Flight(double duration, Location startLocation, Location endLocation)
+// + printTicket(): String
+// + addLuggage(): void
+public Flight(double duration, Location startLocation, Location endLocation){
+    ;
+}
+public String printTicket(){
+    ;
+}
+public void addLuggage(){
+    ;
+}
+
 }
--- Hotel.java
+++ Hotel.java
@@ -1,3 +1,20 @@
 public class Hotel {
-    
+//     - roomNumber: int
+// - dateArriving: Date
+// - dateLeaving: Date
+// - roomNumber: String
+// - capacity: int
+// - numOfBeds
+private int roomNumber;
+private Date dateArriving;
+private Date dateLeaving;
+private String roomNumber;
+private int capacity;
+private int numOfBeds;
+
+//+ printTicket(): String
+public String printTicket(){
+    ;
+}
+
 }
