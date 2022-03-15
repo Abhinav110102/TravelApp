98b9a7be21a932854e2a87c5fa03f34463c1b42e
M-Valentino <79779618+M-Valentino@users.noreply.github.com>
2022-03-15 12:32:35 -0400

    Create Rating.java

---
+++ Rating.java
@@ -0,0 +1,12 @@
+public class Rating {
+  private String username;
+  private int rating;
+  
+  public Rating(String Username, int rating, boolean isNameVisible) {
+    ;
+    // import star unicode and do for loop
+  }
+  public HideName() {
+  ;}
+}
+
