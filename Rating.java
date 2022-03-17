d1c99f874ce6688e2f6cb9396fd5cc92749ae8e4
Mark Valentino <markvalentino@Marks-MacBook-Pro.local>
2022-03-17 12:08:43 -0400

    pretty much finalized Rating class -Mark

--- Rating.java
+++ Rating.java
@@ -1,13 +1,17 @@
 public class Rating {
   private String username;
   private int rating;
+  // False means name is not visible.
   private boolean isNameVisible;
   
-  public Rating(String Username, int rating, boolean isNameVisible) {
-    ;
-    // import star unicode and do for loop
+  public Rating(String username, int rating, boolean isNameVisible) {
+    this.username = username;
+    this.rating = rating;
+    this.isNameVisible = isNameVisible;
   }
+
   public void HideName() {
-  ;}
+    isNameVisible = false;
+  }
 }
 
