1bf71c078edcb219f7818a3b4af58b2efd64d135
Abhinav110102 <abhinavmyadala@gmail.com>
2022-03-21 00:39:54 -0400

    Some more stubs

---
+++ AllUsers.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class AllUsers {
+    
+// -allUsers: AllUsers
+// -users: ArrayList<User>
+private AllUsers allUsers;
+private ArrayList<User> users;
+// - AllUsers()
+// +  getInstance(): AllUsers
+// + addUser(String firstName, String lastName, String phoneNumber, String ID, ): void
+// + logout: void
+private AllUsers(){
+    
+}; // is this  correct?
+public AllUsers getInstance(){
+    ;
+}
+public void addUser(String firstName,String lastName, String phoneNumber,String ID){
+    ;
+}
+public void logout(){
+    ;
+}
+
+
+}
--- TravelApplication.java
+++ TravelApplication.java
@@ -1,3 +1,80 @@
 public class TravelApplication {
-    
+//     - users: AllUsers
+// - flights: AllFlights
+// - hotels: AllHotels
+// - currentUser: User
+// - database: Database
+private AllUsers users;
+private AllFlights flights;
+private AllHotels hotels;
+private User currentUser;
+private Database database;
+
+
+// - TravelApplication()
+// + getInstance(): TravelApplication()
+// - login(String username, String password): String
+// - signUp(String username, String password): String
+// - logout(): String
+// + addUser(String age): void
+// + Search(String input): Ticket
+// + Booking(Ticket): void 
+// - addTicketToUser(Ticket): void
+// - updateTicket(Flight): void
+// - updateTicket(Hotel): void
+// - printTickets(): String
+// + displayOptions(): String
+// + inputOption(int input): String
+// + displayAvailableMonth(int month): void
+// + displayUserID(): String
+
+//private TravelApplication();
+public static TravelApplication getInstance(){
+    ;
+}
+private String login(){
+    ;
+}
+private String login(String username,String password) {
+    ;
+}
+private String signUp(String username,String password){
+    ;
+}
+private String Logout(){
+    ;
+}
+public void addUser(String age){
+    ;
+}
+public Ticket Search(String input){
+    ;
+}
+public void Booking(Ticket){
+    ;
+}
+private void addTicketToUser(Ticket){
+    ;
+}
+private void updateTicket(Flight){
+    ;
+}
+private void updateTicket(Hotel){
+    ;
+}
+private String printTicket(){
+    ;
+}
+public String displayOptions(){
+    ;
+}
+public String inputOption(int input){
+    ;
+}
+public void displayAvailableMonth(int month){
+    ;
+}
+public String displayUserID(){
+    ;
+}
 }
