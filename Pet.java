43feec59ff9d0532349bd679d50f4975462efe98
Mark Valentino <markvalentino@Marks-MacBook-Pro.local>
2022-03-22 10:26:56 -0400

    almost finished Pet.java

--- Pet.java
+++ Pet.java
@@ -1,14 +1,31 @@
+/**
+ * Class for managing a Pet and determining if a ticket is needed
+ * for the pet.
+ * @author Mark Valentino
+ * //Should I put Abi as stub creator?
+ */
 public class Pet {
-  //  - weight: int
-  private int weight;
 
-//   + Pet(String name, int weight)
-// + ticketNeeded(): bool
-public Pet(String name , int weight){
- ;
-}
-public boolean ticketNeeded(){
-    ;
-}
+  private int weight;
+  private String name; // ADD NAME TO UML
+
+  /**
+   * Constructor
+   */
+  public Pet(String name , int weight){
+    this.name = name;
+    this.weight = weight;
+  }
+
+  /**
+   * Method to determine wether or not a ticket is needed
+   * since pets above a certain weight need a ticket.
+   * @return true if wieght > 16
+   */
+  public boolean ticketNeeded(){ // NEED EXACT WEIGHT
+    if (weight > 16) {
+      return true;
+    }
+  }
 }
 
