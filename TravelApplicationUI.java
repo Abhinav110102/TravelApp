f211ffa59ebb951645a9a01157c451889f994ac4
Abhinav110102 <69817378+Abhinav110102@users.noreply.github.com>
2022-03-22 12:32:35 -0400

    Update TravelApplicationUI.java

---
+++ TravelApplicationUI.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+// - scanner: Scanner
+public class TravelApplicationUI {
+    private Scanner scanner;
+    
+// + TravelApplicationUI()
+// + run(): void
+// + printOptions(): void
+// + main(): void
+public TravelApplicationUI(){
+    ;
+}
+public void run(){
+    ;
+}
+public void printOptions(){
+    ;
+}
+public void main(){
+    ;
+}
+
+
+}
