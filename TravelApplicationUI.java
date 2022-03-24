1e0082312615f0e613e83ef5c7baa78d767efc07
M-Valentino <79779618+M-Valentino@users.noreply.github.com>
2022-03-24 12:56:36 -0400

    actually runs now with error:
    Exception in thread "main" java.util.NoSuchElementException
    	at java.util.Scanner.throwFor(Scanner.java:862)
    	at java.util.Scanner.next(Scanner.java:1485)
    	at java.util.Scanner.nextInt(Scanner.java:2117)
    	at java.util.Scanner.nextInt(Scanner.java:2076)
    	at TravelApplicationUI.run(TravelApplicationUI.java:20)
    	at TravelApplicationUI.main(TravelApplicationUI.java:55)

--- TravelApplicationUI.java
+++ TravelApplicationUI.java
@@ -15,7 +15,7 @@ public class TravelApplicationUI {
         int option = 0;
         System.out.println("Welcome to our Booking App!");
         while (true) {
-            printOptions()
+            printOptions();
             System.out.println("What would you like to do?:");
             option = scanner.nextInt();
 
@@ -40,7 +40,6 @@ public class TravelApplicationUI {
         
 
         
-    }
     public void printOptions(){
         System.out.println("************ Main Menu ************");
         System.out.println("1. Login");
@@ -48,11 +47,12 @@ public class TravelApplicationUI {
         System.out.println("3. Book a Flight");
         System.out.println("4. Book a Hotel");
         System.out.println("5. Quit app");
-
+        
     }
 
     public static void main(String[] args) {
-        run();
+        TravelApplicationUI travelApplicationUI = new TravelApplicationUI();
+        travelApplicationUI.run();
 
     }
 
