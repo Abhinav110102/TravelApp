4740c24560251586ba0cb31da90d5a5d12f1a603
M-Valentino <79779618+M-Valentino@users.noreply.github.com>
2022-03-30 22:16:49 -0400

    added javadocs

--- Location.java
+++ Location.java
@@ -1,15 +1,17 @@
+/**
+ * Class for managing data in a location.
+ */
 public class Location {
-// - Street: String
-// - City: String
-// - State:  String
-// - zipcode: String
-// - country: String
+
     private String street;
     private String city;
     private String state;
     private String zipcode;
     private String country;
 
+    /**
+     * Constructor
+     */
     public Location(String street, String city, String state, String zipcode, String country) {
         this.street = street;
         this.city = city;
@@ -17,28 +19,49 @@ public class Location {
         this.zipcode = zipcode;
         this.country = country;
     }
-
+    /**
+     * Method to retun the street
+     * @return the street to be returned.
+     */
     public String getStreet() {
         return street;
     }
 
+    /**
+     * Method to retun the city
+     * @return the city to be returned.
+     */
     public String getCity() {
         return city;
     }
 
+    /**
+     * Method to retun the state
+     * @return the state to be returned.
+     */
     public String getState() {
         return state;
     }
 
+    /**
+     * Method to retun the zipcode
+     * @return the zipcode to be returned.
+     */
     public String getZipcode() {
         return zipcode;
     }
 
+    /**
+     * Method to retun the country
+     * @return the country to be returned.
+     */
     public String getCountry() {
         return country;
     }
 
-    // + toString(): String
+    /**
+     * toString() method
+     */
     public String toString(){
         return this.street + "\n" + this.city + ", " + this.state + " " + this.zipcode + "\n" + this.country;
     }
