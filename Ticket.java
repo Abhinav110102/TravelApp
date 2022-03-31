261ee7c6df33717c8f04989f5715d7634fa78463
M-Valentino <79779618+M-Valentino@users.noreply.github.com>
2022-03-31 16:51:10 -0400

    added javadocs

--- Ticket.java
+++ Ticket.java
@@ -1,21 +1,24 @@
 import java.util.ArrayList;
 import java.lang.Math;
 
+/**
+ * Class for managing data in a Ticket which is added to a user. This class is a parent of FlightTicket and HotelTicket.
+ */
 public abstract class Ticket {
     protected String ID;
     protected String Company;
     protected Location location;
     protected ArrayList<Rating> ratings;
 
-    // + printTicket(): String
-    // + displayAvailable(): String
-    // + addRating(): void
-    // + getAvgRatings(): int
-
     public abstract String printTicket(); // Print to txt
     public abstract Ticket bookTicket();
-    //public abstract String displayAvailable();
 
+    /**
+     * Method to add a rating created by a user to  array list of ratings.
+     * @param username the username on a rating
+     * @param rating the rating numerical value on a rating
+     * @param isNameVisible determines wether a user's name is visible
+     */
     public void addRating(String username, int rating, boolean isNameVisible) {
         Rating rat = new Rating(username, rating, isNameVisible);
         //if (this.ratings.contains(rat)) {
@@ -24,6 +27,10 @@ public abstract class Ticket {
         this.ratings.add(rat);
     }
 
+    /**
+     * Method to calculate average value of ratings.
+     * @return ret the average rating value.
+     */
     public double getAvgRatings() {
         double ret = 0;
         for (Rating i : this.ratings) {
@@ -35,6 +42,10 @@ public abstract class Ticket {
         return ret;
     }
 
+    /**
+     * Method to get location.
+     * @return location the location to be returned.
+     */
     public Location getLocation() {
         return location;
     }
