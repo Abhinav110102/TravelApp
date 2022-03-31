b4207a36f49e8de66a6c07e66aff4ddfb7fcd3f5
M-Valentino <79779618+M-Valentino@users.noreply.github.com>
2022-03-30 22:43:58 -0400

    added javadocs

--- AllLuggages.java
+++ AllLuggages.java
@@ -1,33 +1,56 @@
 import java.util.ArrayList;
 
+/**
+ * Class for managing data in a set of all luggage stored in a User.
+ */
 public class AllLuggages {
-// -allFlights: AllFlights
-// -flights: ArrayList<Flight
-private static AllLuggages allLuggages = null;
-private static ArrayList<Luggage> luggages = new ArrayList<>();
+
+    private static AllLuggages allLuggages = null;
+    private static ArrayList<Luggage> luggages = new ArrayList<>();
 
 // -AllFlights()
 // +  getInstance(): AllFlights
 // + addFlight(): void
-// +logout: void
-private AllLuggages(){}
 
-public static AllLuggages getInstance(){
-    if (allLuggages == null) {
-        allLuggages = new AllLuggages();
+    /**
+     * Constructor
+    */
+    private AllLuggages(){}
+
+    /**
+    * Method to return an instance of itself
+    * @return AllLuggages the instance of itself to be returned.
+    */
+    public static AllLuggages getInstance(){
+        if (allLuggages == null) {
+          allLuggages = new AllLuggages();
+        }
+        return allLuggages;
     }
-    return allLuggages;
-}
 
-public ArrayList<Luggage> getLuggages() {
-    return luggages;
-}
+    /**
+    * Method to return an array list of Luggage objects.
+    * @return luggages the array list to be returned.
+    */
+    public ArrayList<Luggage> getLuggages() {
+      return luggages;
+    }
 
-public void addLuggage(String userID, String flightID, double weight){
-    Luggage luggage = new Luggage(userID, flightID, weight);
-    luggages.add(luggage);
-}
-public static void logout(){
-    DataWriter.saveLuggages();
-}
+    /**
+    * Method to add a Luggage to the array list of luggages
+    * @param userID a constructor value in a Luggage
+    * @param flightID a constructor value in a Luggage
+    * @param weight a constructor value in a Luggage
+    */
+    public void addLuggage(String userID, String flightID, double weight){
+        Luggage luggage = new Luggage(userID, flightID, weight);
+        luggages.add(luggage);
+    }
+
+    /**
+    * Method to save all luggages to JSON.
+    */
+    public static void logout(){
+        DataWriter.saveLuggages();
+    }
 }
