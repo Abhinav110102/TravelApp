99860992197ee7878b4748d768cce107da5374f0
Samuel Godfrey <sjg6@email.sc.edu>
2022-04-10 13:14:55 -0400

    Filled some methods in testers

--- LuggageTest.java
+++ LuggageTest.java
@@ -1,5 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LuggageTest {
     public void testIsOver50() {
-        ;
+        Luggage luggage = new Luggage("djsgh4574976", "jgid458745798", 55);
+        assertTrue(luggage.isOver50(luggage.getWeight()));
+    }
+
+    public void testIsNotOver50() {
+        Luggage luggage = new Luggage("djsgh4574976", "jgid458745798", 20);
+        assertFalse(luggage.isOver50(luggage.getWeight()));
     }
 }
--- PetTest.java
+++ PetTest.java
@@ -1,5 +1,11 @@
 public class PetTest {
-    public void testPetWeight() {
-        ;
+    public void testPetTicketNeeded() {
+        Pet pet = new Pet("Lance", 37, 12);
+        assertTrue(pet.ticketNeeded());
+    }
+
+    public void testPetTicketNotNeeded() {
+        Pet pet = new Pet("Vance", 33, 8);
+        assertFalse(pet.ticketNeeded());
     }
 }
