422422fe01202c87ee66ac79f1583c613bf05ad5
Samuel Godfrey <sjg6@email.sc.edu>
2022-04-10 12:11:36 -0400

    Name Changes

--- AllHotelsTester.java
+++ AllHotelsTester.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class HotelsTester {
+public class AllHotelsTester {
     private AllHotels hotels = AllHotels.getInstance();
     private ArrayList<Hotel> hotelList = hotels.getHotels();
 	
