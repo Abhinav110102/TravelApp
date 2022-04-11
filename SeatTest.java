8b8744ace06f8038dec9860eeedcbf9cfb05c259
M-Valentino <79779618+M-Valentino@users.noreply.github.com>
2022-04-10 21:14:19 -0400

    test runs now

--- SeatTest.java
+++ SeatTest.java
@@ -8,34 +8,14 @@ import org.junit.jupiter.api.Test;
 
 public class SeatTest {
     
-    @BeforeClass
-	public static void oneTimeSetup() {
-		
-	}
-	
-	@AfterClass
-	public static void oneTimeTearDown() {
-		
-	}
-	
-	@BeforeEach
-	public static void setup() {
-		//runs before each test
-	}
-	
-	@AfterEach
-	public static void tearDown() {
-		//runs after each test
-	}
+    private Seat seat;
 
 	@Test
 	public void setColumnTest(){
 		// In Seat.java
-		Seat seat = new Seat(-1, 0, false);
+		seat = new Seat(-1, 0, false);
 		String letter = seat.setColumn(-1);
 		assertEquals(null, letter, "Should not set a letter.");
 	}
 
-
-    
 }
