d99fa8500ac88409f7d3a6c7a03f5a7f44c79caa
M-Valentino <mike9377mike9377@gmail.com>
2022-04-10 21:29:48 -0400

    trying to get test to work

--- HotelTest.java
+++ HotelTest.java
@@ -9,38 +9,24 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class HotelTest {
-    
-    @BeforeClass
-	public static void oneTimeSetup() {
-		
-	}
-	
-	@AfterClass
-	public static void oneTimeTearDown() {
-		
-	}
+
+	private Hotel hotel;
 	
 	@BeforeEach
-	public static void setup() {
-		//runs before each test
-	}
-	
-	@AfterEach
-	public static void tearDown() {
-		//runs after each test
-	}
-
-	@Test
-	public void getHotelAddressTest(){
+	public void setup() {
 		ArrayList<String> address = new ArrayList<String>();
 		address.add("a");
 		address.add("a");
 		address.add("a");
 		address.add("a");
 		address.add("a");
-		Hotel hotel = new Hotel("a", "a", "a", address,
+		hotel = new Hotel("a", "a", "a", address,
 		1, 1, 1, 1, "a", "a", false, 5.0);
-		
+	}
+
+	@Test
+	public void getHotelAddressTest(){
+		setup();	
 		assertTrue(hotel.location.getCity().equals("a") &&
 		hotel.location.getCountry().equals("a") &&
 		hotel.location.getState().equals("a") &&
