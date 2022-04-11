b7580c63829b3b77e8c5084900e4d666aada1e29
Samuel Godfrey <sjg6@email.sc.edu>
2022-04-10 20:12:40 -0400

    Added test flags to some test classes

--- PetTest.java
+++ PetTest.java
@@ -1,11 +1,18 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class PetTest {
+    @Test
     public void testPetTicketNeeded() {
         Pet pet = new Pet("Lance", 37, 12);
         assertTrue(pet.ticketNeeded());
     }
-
+    @Test
     public void testPetTicketNotNeeded() {
         Pet pet = new Pet("Vance", 33, 8);
         assertFalse(pet.ticketNeeded());
